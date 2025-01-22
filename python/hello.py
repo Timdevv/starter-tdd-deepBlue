45fbfd2a04cccb223f8088ebd61326dc9255456e
Izerty <timothee.fougeron@gmail.com>
2025-01-22 15:38:53 +0100

    test front & backward
    (●'◡'●)

--- python/hello.py
+++ python/hello.py
@@ -1,2 +1,31 @@
-def get_answer():
-    return 42
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Rover:
+    def __init__(self, position, direction):
+        self.position = position
+        self.direction = direction
+    
+    def move_forward(self):
+        if self.direction == "North":
+            self.position.y += 1
+        elif self.direction == "South":
+            self.position.y -= 1
+        elif self.direction == "East":
+            self.position.x += 1
+        elif self.direction == "West":
+            self.position.x -= 1
+    
+    def move_backward(self):
+        if self.direction == "North":
+            self.position.y -= 1
+        elif self.direction == "South":
+            self.position.y += 1
+        elif self.direction == "East":
+            self.position.x -= 1
+        elif self.direction == "West":
+            self.position.x += 1
+
